610131325cee9ee40c15b380903957921e537e79
Chi Trung Nguyen <napcae@gmail.com>
2013-10-31 13:22:50 +0100

    Fixing "locale" error for iterm 2 mavericks.

--- source/50_prompt.sh
+++ source/50_prompt.sh
@@ -41,3 +41,7 @@ else # normal
     PS1="$hostncolor\h\[\e[0;38m\]:\[\e[0;32m\]\w \[\e[0;36m\]\u\[\e[0;37m\]\$ \[\033[0m\]"
 fi
 
+##http://www.bergspot.com/blog/2012/02/how-to-fix-warning-setlocale-lc_ctype-cannot-change-locale-utf-8/
+##manual locale set
+export LC_CTYPE="de_DE.UTF-8"
+
