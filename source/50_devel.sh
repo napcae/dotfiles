b04ffa2b238410c6433a4afc69de19c3e4b557a4
Chi Trung Nguyen <napcae@gmail.com>
2014-02-17 14:25:31 +0000

    removing rbenv stuff

--- source/50_devel.sh
+++ source/50_devel.sh
@@ -91,9 +91,9 @@
 #}
 
 # rbenv init.
-PATH=$(path_remove ~/.dotfiles/libs/rbenv/bin):~/.dotfiles/libs/rbenv/bin
-PATH=$(path_remove ~/.dotfiles/libs/ruby-build/bin):~/.dotfiles/libs/ruby-build/bin
+#PATH=$(path_remove ~/.dotfiles/libs/rbenv/bin):~/.dotfiles/libs/rbenv/bin
+#PATH=$(path_remove ~/.dotfiles/libs/ruby-build/bin):~/.dotfiles/libs/ruby-build/bin
 
-if [[ "$(type -P rbenv)" && ! "$(type -t _rbenv)" ]]; then
-  eval "$(rbenv init -)"
-fi
+#if [[ "$(type -P rbenv)" && ! "$(type -t _rbenv)" ]]; then
+#  eval "$(rbenv init -)"
+#fi
