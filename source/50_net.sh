dfa1c6b4eebf9dde9dcde74f55446d6ed9bd0ddc
Chi Trung Nguyen <napcae@fastmail.fm>
2014-02-20 18:24:09 +0100

    adding mosh support for sslh

--- source/50_net.sh
+++ source/50_net.sh
@@ -8,3 +8,5 @@ alias flush="dscacheutil -flushcache"
 # View HTTP traffic
 alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
 
+#adding mosh sslh support
+alias mosh="mosh --bind-server=any "
