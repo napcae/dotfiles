a91c192a351de6b46d8aedd7b84e56d78fb0fda4
Chi Trung Nguyen <napcae@fastmail.fm>
2014-04-01 11:56:35 +0200

    adding english locales for anapnea

--- source/60_anapnea.sh
+++ source/60_anapnea.sh
@@ -2,3 +2,19 @@
 
 #Gem executable path for anapnea
 PATH=$PATH:/home/napcae/.gem/ruby/2.0.0/bin 
+
+LANG=en_GB.UTF-8
+LC_CTYPE="en_GB.UTF-8"
+LC_NUMERIC="en_GB.UTF-8"
+LC_TIME="en_GB.UTF-8"
+LC_COLLATE="en_GB.UTF-8"
+LC_MONETARY="en_GB.UTF-8"
+LC_MESSAGES="en_GB.UTF-8"
+LC_PAPER="en_GB.UTF-8"
+LC_NAME="en_GB.UTF-8"
+LC_ADDRESS="en_GB.UTF-8"
+LC_TELEPHONE="en_GB.UTF-8"
+LC_MEASUREMENT="en_GB.UTF-8"
+LC_IDENTIFICATION="en_GB.UTF-8"
+LC_ALL=en_GB.UTF-8
+
