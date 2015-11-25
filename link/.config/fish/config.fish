740865254dccefa0241c8b62f393006ef754f2da
Chi Trung Nguyen <napcae@fastmail.fm>
2015-11-25 04:15:31 +0100

    adding chrome terminal handler

--- link/.config/fish/config.fish
+++ link/.config/fish/config.fish
@@ -13,3 +13,12 @@ rvm default
 function subl
     open -a "Sublime Text 2.app" $argv
 end
+
+function chrome
+   if count $argv > /dev/null
+     /opt/homebrew-cask/Caskroom/google-chrome/latest/Google\ Chrome.app/Contents/MacOS/Google\ Chrome $argv;
+     open /opt/homebrew-cask/Caskroom/google-chrome/latest/Google\ Chrome.app
+   else  
+     open /opt/homebrew-cask/Caskroom/google-chrome/latest/Google\ Chrome.app
+   end  
+end
