b1aed921d797ec2d607b2f4303ac31a810b28a04
Chi Trung Nguyen <git@ctnguyen.net>
2018-09-26 09:30:53 +0200

    tail 7 for whitespace

--- link/.config/fish/functions/mfa.fish
+++ link/.config/fish/functions/mfa.fish
@@ -1,3 +1,4 @@
+# Defined in /var/folders/0y/pcc55wz96nv3t7kr_s4n8n5w0000gn/T//fish.ez2Zl1/mfa.fish @ line 2
 function mfa
-	ykman oath code $argv | tail -c 6 | pbcopy
+	ykman oath code $argv | tail -c 7 | pbcopy
 end
