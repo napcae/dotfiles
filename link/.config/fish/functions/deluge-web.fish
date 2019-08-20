28c7e834d337a556fb8afb62b170384eb01883e6
Chi Trung Nguyen <git@ctnguyen.net>
2019-08-20 09:03:25 +0200

    latest changes

--- link/.config/fish/functions/deluge-web.fish
+++ link/.config/fish/functions/deluge-web.fish
@@ -1,4 +1,4 @@
-# Defined in /var/folders/0y/pcc55wz96nv3t7kr_s4n8n5w0000gn/T//fish.zoIhD2/deluge-web.fish @ line 2
+# Defined in /var/folders/j_/8cpj3cg1575_6lkj8tqpwzlc0000gn/T//fish.pWGAPG/deluge-web.fish @ line 2
 function deluge-web
-	ssh -fNL 8112:localhost:8112 -T napcae@twix && chrome --args --profile-directory="Profile 1" http://localhost:8112
+	ssh -fNL 8112:localhost:8112 -T milka && chrome --args --profile-directory="Profile 1" http://localhost:8112
 end
---
+++ link/.config/fish/functions/please.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function please --description 'alias please=sudo'
+	sudo  $argv;
+end
