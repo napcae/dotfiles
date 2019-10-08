5267ea92083e891f5c6ccb953a843194ab9f2c8d
Chi Trung Nguyen <git@ctnguyen.net>
2019-10-08 15:12:33 +0200

    add mp3d fish command

--- link/.config/fish/functions/mp3d.fish
+++ link/.config/fish/functions/mp3d.fish
@@ -1,8 +1,10 @@
-# Defined in /var/folders/0y/pcc55wz96nv3t7kr_s4n8n5w0000gn/T//fish.hCiBMD/mp3d.fish @ line 2
+# Defined in /var/folders/j_/8cpj3cg1575_6lkj8tqpwzlc0000gn/T//fish.77Y4oW/mp3d.fish @ line 2
 function mp3d
-	if contains youtube $argv; 
+	if string match -q "*youtube*" $argv; 
+        echo "Downloading from youtube"
 		youtube-dl -f bestaudio[ext=m4a] --embed-thumbnail --add-metadata "$argv"
 	else
+        echo "Downloading non-youtube"
 		youtube-dl --embed-thumbnail --add-metadata "$argv"
 	end
 end
